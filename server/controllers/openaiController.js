711aa73de8e35520dac31b39688d7246b891f592
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-03-13 12:18:38 -0400

    Updated openai prompt Co-authored-by: rsbaek <rsbaek@users.noreply.github.com>

--- server/controllers/openaiController.js
+++ server/controllers/openaiController.js
@@ -15,8 +15,8 @@ openaiApiController.userQuery = async (req, res, next) => {
     const prompt = `
     You are a genius at writing prompts for the google places api.
     You must create a prompt that will contain a query and a location. example: i want pizza near new york.
-    You are going to receieve a query.
-    You are going to receieve a location.
+    You are going to receive a query.
+    You are going to receive a location.
     Your response must be one sentence long.
     Your response must be a string.
     Do not include markdown in your response
@@ -42,35 +42,66 @@ openaiApiController.userQuery = async (req, res, next) => {
 openaiApiController.createResponse = async (req, res, next) => {
   try {
     const { googleResponse, gptResponse } = res.locals;
+    const jsonGoogleResponse = JSON.stringify(googleResponse)
     const prompt = `
     You are an expert at using the Google Places API. 
     You are an expert at recommending activities.
     You will be given an array of objects.
-    These objects will contain fields such as:
+    The format of the array of objects will be like:
+    [
+    {types: ['type 1','type 2']
+    formattedAddress: 'address',
+    rating: number
+    googleMapsUri: 'url'
+    priceLevel: 'price level'
+    displayName: { text: 'name', languageCode: 'language' }
+    }
+  ]
 
-    places.displayName,
-    places.formattedAddress,
-    places.priceLevel,
-    places.rating,
-    places.types,
-    places.googleMapsUri
-    
-    You will be given a user query.
+  For example, if one of the objects in the given array looks like this:
+
+      [{
+    types: [
+      'restaurant',
+      'pub',
+      'bar',
+      'food',
+      'point_of_interest',
+      'establishment'
+    ],
+    formattedAddress: '1407 N Orange Ave, Orlando, FL 32804, USA',
+    rating: 4.7,
+    googleMapsUri: 'https://maps.google.com/?cid=12957464324064300201',
+    priceLevel: 'PRICE_LEVEL_INEXPENSIVE',
+    displayName: { text: 'Gnarly Barley', languageCode: 'en' }
+    }
+  ]
+  You will be given a user query.
+   The format of the user query will be a string that includes a query and a location. For example:
+    "Find cheap restaurants in Florida where I can grab a bite to eat."
+    You should recognize the relevance of key phrases such as:
+    "cheap" in favor of 'PRICE_LEVEL_INEXPENSIVE'
     Your objective is to decide the 3 best activities to recommend based on the user query.
+    Your response should only be an array of objects
+    Your response must recommend 3 objects from this user query.
+    Your must not change the information inside of the user query.
+    Your response must be formatted like this the same as the array of objects
     Do not include any markdown in your response.
-    Your response must be an array of objects.
-    Your response must maintain the same format that you were given, and leave the top 3 objects within the array that best align with the user query.
-    The keys in your response should not be in quotes, example: places.rating: 4.5
-    array of objects: ${googleResponse}
+    
+    array of objects: ${jsonGoogleResponse}
     user query: ${gptResponse}
     `;
+    console.log('gptResponse', gptResponse);
     const result = await openai.chat.completions.create({
       model: 'gpt-4o-mini',
       store: true,
       messages: [{ role: 'user', content: prompt }],
       temperature: 0.2,
     });
-    console.log('THIS IS GONNA BE AN ARRAY OF 3 OBJECTS', result.choices[0].message.content);
+    console.log(
+      'THIS IS GONNA BE AN ARRAY OF 3 OBJECTS',
+      result.choices[0].message.content
+    );
     res.locals.gptFields = result.choices[0].message.content;
     return next();
   } catch (error) {

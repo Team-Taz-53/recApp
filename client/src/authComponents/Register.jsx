edc36eb41cee436d057454c3e65b6b5ce608c5e3
Rachel <rsbaek98@gmail.com>
2025-03-13 19:41:38 -0400

    login and registration page scaffolding done

---
+++ client/src/authComponents/Register.jsx
@@ -0,0 +1,96 @@
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [location, setLocation] = useState("");
+
+  const regRequest = async () => {
+    try {
+    console.log("regRequest entered")
+      if (!username || !password || !location) {
+        console.error("Registration failed: Empty fields.");
+        alert("Username and password cannot be empty.");
+        return;
+      }
+      const response = await fetch("http://localhost:3000/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          location: location,
+        }),
+      });
+      if (response.ok) {
+        console.log("username: ", username);
+        console.log("password: ", password);
+        console.log("location: ", location);
+        console.log("You are logged in");
+        navigate("/dashboard", {
+          state: { username: `${username}` },
+        });
+      } else {
+        const error = await response.json();
+        console.error('Login failed', error);
+        alert('Login failed');
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+      alert("Network error: Please try again later.");
+    }
+  };
+  return (
+    <div>
+      <h1>Register</h1>
+      <div className="inputs">
+        <label className="label" htmlFor="username">
+          Username
+        </label>
+        <input
+          id="username"
+          type="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="inputs">
+        {" "}
+        <label className="label" htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="inputs">
+        {" "}
+        <label className="label" htmlFor="location">
+          Location
+        </label>
+        <input
+          id="location"
+          type="location"
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
+      <div className="card">
+        {/* onclick, this should update the refs  */}
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            regRequest();
+          }}
+        >
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppAlpha from './AppAlpha'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppAlpha />
   </StrictMode>,
 )

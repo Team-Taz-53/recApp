787e9422847933df7fa252492e6087afe3d380cd
Rachel <rsbaek98@gmail.com>
2025-03-13 17:10:32 -0400

    backend and authentication done

--- server/routers/routers.js
+++ server/routers/routers.js
@@ -1,29 +1,36 @@
 //Boilerplate imports------------------------------
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // const authenticationController = require('../controllers/authenticationController');
-const googleApiController = require('../controllers/googleApiController');
-const userQueryController = require('../controllers/userQueryController');
-const openaiApiController = require('../controllers/openaiController');
+const googleApiController = require("../controllers/googleApiController");
+const userQueryController = require("../controllers/userQueryController");
+const openaiApiController = require("../controllers/openaiController");
+const authenticationController = require("../controllers/authenticationController")
 
 // //post request to handle logins -> tested and works
-// router.post('/login', authenticationController.login, (req, res) => {
-//   return res.status(200).send('This is the working login button');
-// });
+router.post('/login', authenticationController.login, (req, res) => {
+  return res.status(200).send('This is the working login button');
+});
 
-// //post request to handle logins -> tested and works
-// router.post('/register', authenticationController.register, (req, res) => {
-//   return res.status(200).send('This is the working register submit button');
-// });
+router.post("/register", authenticationController.register, (req, res) => {
+  return res.status(200).send("This is the working register submit button");
+});
+
+router.post("/clear", authenticationController.delete, (req, res) => {
+  return res.status(200).send("Cleared the database.");
+})
 
 router.post(
-  '/userquery', userQueryController.parseQuery, openaiApiController.userQuery, googleApiController.getEvents, openaiApiController.createResponse,
+  "/userquery",
+  userQueryController.parseQuery,
+  openaiApiController.userQuery,
+  googleApiController.getEvents,
+  openaiApiController.createResponse,
   (req, res) => {
-    return res
-      .status(200)
-      .send(res.locals.gptFields)
+    return res.status(200).send(res.locals.gptFields);
   }
 );
+
 
 //Export the router---------------------------
 module.exports = router;

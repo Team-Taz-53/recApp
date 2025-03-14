43cd66ed3ff80167947e9a9920341a9ffee391ff
Rachel <rsbaek98@gmail.com>
2025-03-14 11:13:05 -0400

    data.places updated for latency

--- server/controllers/googleApiController.js
+++ server/controllers/googleApiController.js
@@ -27,8 +27,10 @@ googleApiController.getEvents = async (req, res, next) => {
     data.places.map((place) => {
       if (place.photos && place.photos.length > 0) {
         place.photoUrl = `https://places.googleapis.com/v1/${place.photos[0].name}/media?maxWidthPx=400&key=${process.env.GOOGLE_API_KEY}`;
+        place.photos = {};
       }
     });
+    console.log(data)
     // console.log('data.places.photos', data.places);
     res.locals.googleResponse = data;
     return next();
